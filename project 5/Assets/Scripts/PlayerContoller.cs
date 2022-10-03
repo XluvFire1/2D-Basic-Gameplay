313342a2cfb0aa3071e523f0b0fa64b99e18c8ea
Jacob Zambrano <jacobzambrano858@riversideunified.org>
2022-10-03 11:28:46 -0700

    Enemy, Player, Projectille (Shooting)

---
+++ project 5/Assets/Scripts/PlayerContoller.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerContoller : MonoBehaviour
+{
+    public float moveSpeed = 5;
+
+    public float xRange = 10;
+
+    private float _horizontalInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _horizontalInput = Input.GetAxis("Horizontal");
+
+        transform.Translate(Vector3.right * _horizontalInput * moveSpeed * Time.deltaTime);
+
+        if(transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+
+        if(transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+    }
+}
