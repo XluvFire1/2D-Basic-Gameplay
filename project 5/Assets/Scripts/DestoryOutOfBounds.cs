aefaa309d8c0d332fbe7e30a0c3c5fac94cbfcb4
Jacob Zambrano <jacobzambrano858@riversideunified.org>
2022-10-05 11:01:21 -0700

    enemy's

--- project 5/Assets/Scripts/DestoryOutOfBounds.cs
+++ project 5/Assets/Scripts/DestoryOutOfBounds.cs
@@ -14,5 +14,10 @@ public class DestoryOutOfBounds : MonoBehaviour
         {
             Destroy(this.gameObject);
         }
+
+        if(transform.position.y < lowerBound)
+        {
+            Destroy(this.gameObject);
+        }
     }
 }
